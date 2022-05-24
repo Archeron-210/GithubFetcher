f3ef54881c15df219767844842fec193b3b0b40c
Laura Jacquin <laura.jcqn@live.fr>
2022-05-24 16:25:19 +0200

    started Xcode project, added groups to organize files

---
+++ GithubFetcher/GithubFetcher/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubFetcher
+//
+//  Created by Archeron on 24/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
