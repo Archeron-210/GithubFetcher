a89b5f1a57d7f54995451852b3e8d37564d5d9cb
Laura Jacquin <laura.jcqn@live.fr>
2022-05-25 19:00:01 +0200

    added SearchViewController scene and ResultDetailViewController scene in storyboard, with associated ViewControllers files

---
+++ GithubFetcher/GithubFetcher/Controllers/ResultDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ResultDetailViewController.swift
+//  GithubFetcher
+//
+//  Created by Archeron on 25/05/2022.
+//
+
+import UIKit
+
+class ResultDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
--- GithubFetcher/GithubFetcher/Controllers/SearchViewController.swift
+++ GithubFetcher/GithubFetcher/Controllers/SearchViewController.swift
@@ -1,19 +1,17 @@
 //
-//  ViewController.swift
+//  SearchViewController.swift
 //  GithubFetcher
 //
-//  Created by Archeron on 24/05/2022.
+//  Created by Archeron on 25/05/2022.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class SearchViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
-
