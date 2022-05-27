583aef7ce69b7ebb00078636d41633a92499e48e
Laura Jacquin <laura.jcqn@live.fr>
2022-05-27 19:45:22 +0200

    started working on ResultViewController scene, added a UITableView with cells, started working on cells UI and added a new placeholder image assset

---
+++ GithubFetcher/GithubFetcher/View/ContributorTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ContributorTableViewCell.swift
+//  GithubFetcher
+//
+//  Created by Archeron on 27/05/2022.
+//
+
+import UIKit
+
+class ContributorTableViewCell: UITableViewCell {
+
+    
+
+}
