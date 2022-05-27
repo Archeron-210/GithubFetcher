21593e02a5b23bb3d62c6f1620186e18bab512e2
Laura Jacquin <laura.jcqn@live.fr>
2022-05-27 11:35:00 +0200

    added custom class for RepositoryTableViewCell, setted aspect for TableView in SearchViewController

--- GithubFetcher/GithubFetcher/Controllers/SearchViewController.swift
+++ GithubFetcher/GithubFetcher/Controllers/SearchViewController.swift
@@ -9,18 +9,30 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    // MARK: - Oultlets
+
+    @IBOutlet weak var tableView: UITableView!
+
+
     // MARK: - Properties
 
     let viewModel = SearchViewModel()
     let searchController = UISearchController(searchResultsController: nil)
 
     // MARK: - Lifecycle
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         title = "Search"
         navigationItem.searchController = searchController
+        setTableViewAspect()
+    }
+
+    // MARK: - Private
+
+    private func setTableViewAspect() {
+        tableView.layer.cornerRadius = 15
     }
     
 }
---
+++ GithubFetcher/GithubFetcher/View/RepositoryTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  RepositoryTableViewCell.swift
+//  GithubFetcher
+//
+//  Created by Archeron on 27/05/2022.
+//
+
+import UIKit
+
+class RepositoryTableViewCell: UITableViewCell {
+
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var languageAndStargazersLabel: UILabel!
+
+    
+}
