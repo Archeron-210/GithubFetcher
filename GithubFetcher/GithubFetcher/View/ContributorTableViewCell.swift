b7c765bd41ab814781574338d15b1595ebd259ee
Laura Jacquin <laura.jcqn@live.fr>
2022-05-27 19:56:15 +0200

    started working on ContributorTableViewCell custom class, hooked up outlets and placed markdowns, started working on user profile picture aspect

--- GithubFetcher/GithubFetcher/View/ContributorTableViewCell.swift
+++ GithubFetcher/GithubFetcher/View/ContributorTableViewCell.swift
@@ -9,6 +9,25 @@ import UIKit
 
 class ContributorTableViewCell: UITableViewCell {
 
-    
+    // MARK: - Outlets
+
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var userProfileImageView: UIImageView!
+
+    // MARK: - Life Cycle
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setProfilePictAspect()
+    }
+
+    // MARK: - Configure
+
+
+    // MARK: - Private
+
+    private func setProfilePictAspect() {
+        userProfileImageView.layer.cornerRadius = 64
+    }
 
 }
