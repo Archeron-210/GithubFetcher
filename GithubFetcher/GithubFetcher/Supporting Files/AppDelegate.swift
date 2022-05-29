465548cdde1c8c6fa9122fad1a709ec41113e8e3
Laura Jacquin <laura.jcqn@live.fr>
2022-05-29 17:44:43 +0200

    changed back style of titles in navigation bar to large

--- GithubFetcher/GithubFetcher/Supporting Files/AppDelegate.swift
+++ GithubFetcher/GithubFetcher/Supporting Files/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         UINavigationBar.appearance().tintColor = UIColor.black
+        UINavigationBar.appearance().prefersLargeTitles = true
 
         let navController = UINavigationController()
         let coordinator = MainCoordinator(navigationController: navController)
